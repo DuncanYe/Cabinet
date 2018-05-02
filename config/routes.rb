e51672a7e568b7e9ad7178a4425860ae84138803
Duncan <loxa10077@gmail.com>
2018-05-03 07:36:24 +0800

    fixed authenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'welcome#index'
   get 'welcome/index'
+  resources :docs
 
   authenticated :user do
     root 'docs#index', as: 'authenticated_root'
   end
+  root 'welcome#index'
   # 失敗！！沒效果
-
-  resources :docs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
